1a0b890cfb81d4bda0772b3edbcd3f1dd9570ca9
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-01-27 11:35:08 +0000

    add: first test for default level

--- jasmine-standalone-3.5.0/spec/WildShapeSpec.js
+++ jasmine-standalone-3.5.0/spec/WildShapeSpec.js
@@ -1,5 +1,12 @@
 'use strict'
 
-describe('WildShape', function() {
-  
-})
+describe('wildShape', function() {
+  var wildShape;
+  beforeEach(function() {
+    wildShape = new WildShape();
+  });
+
+  it('starts at level 1', function() {
+    expect(wildShape.level).toEqual(1));
+  });
+});
