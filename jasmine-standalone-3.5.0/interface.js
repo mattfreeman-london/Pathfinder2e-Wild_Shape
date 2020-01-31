2e44d1143cd102af78eb0a511ad3ba6656a41c56
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-01-31 13:55:05 +0000

    add: displays and changes to con mod and str mod

--- jasmine-standalone-3.5.0/interface.js
+++ jasmine-standalone-3.5.0/interface.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var characterLevel = new CharacterLevel();
+  let characterLevel = new CharacterLevel();
   updateLevel();
 
   $('#level-up').on('click', function() {
@@ -19,6 +19,56 @@ $(document).ready(function() {
 
   function updateLevel() {
     $('#character-level').text(characterLevel.level);
+  };
+
+})
+
+$(document).ready(function() {
+  let strengthModifier = new StrengthModifier();
+  updateModifier();
+
+  $('#strength-up').on('click', function() {
+    strengthModifier.strengthUp();
+    updateModifier();
+  })
+
+  $('#strength-down').on('click', function() {
+    strengthModifier.strengthDown();
+    updateModifier();
+  })
+
+  $('#strength-reset').on('click', function() {
+    strengthModifier.reset();
+    updateModifier();
+  });
+
+  function updateModifier() {
+    $('#strength-modifier').text(strengthModifier.modifier);
+  };
+
+})
+
+$(document).ready(function() {
+  let constitutionModifier = new ConstitutionModifier();
+  updateModifier();
+
+  $('#constitution-up').on('click', function() {
+    constitutionModifier.constitutionUp();
+    updateModifier();
+  })
+
+  $('#constitution-down').on('click', function() {
+    constitutionModifier.constitutionDown();
+    updateModifier();
+  })
+
+  $('#constitution-reset').on('click', function() {
+    constitutionModifier.reset();
+    updateModifier();
+  });
+
+  function updateModifier() {
+    $('#constitution-modifier').text(constitutionModifier.modifier);
   };
 
 })
