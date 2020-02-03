97ccf848df73c633b27ea40dda36b5ac99fe51ca
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-02-03 11:44:41 +0000

    placeholder

--- jasmine-standalone-3.5.0/spec/basicUnarmedAttackSpec.js
+++ jasmine-standalone-3.5.0/spec/basicUnarmedAttackSpec.js
@@ -1,12 +1,13 @@
 'use strict'
 
 describe('basicUnarmedAttack', function() {
-  var basicUnarmedAttack;
+  let basicUnarmedAttack;
   beforeEach(function() {
     basicUnarmedAttack = new BasicUnarmedAttack();
   });
 
-  it('defaults to 3, based on a level 1 character, no modifiers, trained', function(){
+  it('defaults to 3, based on a level 1 character, no modifiers, trained', function() {
     expect(basicUnarmedAttack.getCurrentScore()).toEqual(3);
   })
+
 })
